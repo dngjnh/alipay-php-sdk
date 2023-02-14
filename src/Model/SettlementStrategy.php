c1da3c9f5d1917349f4bb20d62156cead2d949f8
dongjinhui <dongjinhui@chicv.com>
2023-02-15 02:20:28 +0800

    feat: SettlementStrategy Model

---
+++ src/Model/SettlementStrategy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Chicv\AlipayPhpSdk\Model;
+
+class SettlementStrategy
+{
+    public $settlementCurrency;
+
+    /**
+     * @return mixed
+     */
+    public function getSettlementCurrency()
+    {
+        return $this->settlementCurrency;
+    }
+
+    /**
+     * @param mixed $settlementCurrency
+     */
+    public function setSettlementCurrency($settlementCurrency)
+    {
+        $this->settlementCurrency = $settlementCurrency;
+    }
+}
