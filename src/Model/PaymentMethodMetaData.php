6bd0f1ab271f64c14ffbb288b50fc89f1549d6c1
dongjinhui <dongjinhui@chicv.com>
2023-02-27 23:49:33 +0800

    feat: blikCode and payerEmail

--- src/Model/PaymentMethodMetaData.php
+++ src/Model/PaymentMethodMetaData.php
@@ -6,6 +6,10 @@ class PaymentMethodMetaData
 {
     public $bankIdentifierCode;
 
+    public $blikCode;
+
+    public $payerEmail;
+
     /**
      * @return mixed
      */
@@ -20,5 +24,39 @@ class PaymentMethodMetaData
     public function setBankIdentifierCode($bankIdentifierCode)
     {
         $this->bankIdentifierCode = $bankIdentifierCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBlikCode()
+    {
+        return $this->blikCode;
+    }
+
+    /**
+     * @param mixed $blikCode
+     * @return void
+     */
+    public function setBlikCode($blikCode)
+    {
+        $this->blikCode = $blikCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayerEmail()
+    {
+        return $this->payerEmail;
+    }
+
+    /**
+     * @param mixed $payerEmail
+     * @return void
+     */
+    public function setPayerEmail($payerEmail)
+    {
+        $this->blikCode = $payerEmail;
     }
 }
